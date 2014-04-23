d82e5ca470f45b77e621602e65a397d0d7919855
Greg Sabia <me@gregsabia.com>
2014-04-23 01:20:31 -0700

    Added GitHub Pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+
+$(function(){
+
+    var router = new Grapnel.Router();
+
+    router.get('docs', function(){
+        $('#documentation').show();
+        $('#basic-usage').hide();
+        $('ul.nav li').removeClass('active').parent().find('li[data-active="documentation"]').addClass('active');
+    });
+
+    router.get('usage', function(){
+        $('#basic-usage').show();
+        $('#documentation').hide();
+        $('ul.nav li').removeClass('active').parent().find('li[data-active="usage"]').addClass('active');
+    });
+
+    if(!router.state){
+        router.anchor.set('usage');
+    }
+}); 
