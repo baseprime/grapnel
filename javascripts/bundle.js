8fd7fc1c2a00206fb7e49c4d5140bc371fe306fd
Greg Sabia Tucker <greg@tucke.rs>
2015-04-01 14:02:06 -0700

    Typo in index

---
+++ javascripts/bundle.js
@@ -0,0 +1,25 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+$(function(){
+
+    var router = new Grapnel({ pushState : true, root : '/Grapnel.js' });
+
+    router.get('/docs', function(){
+        $('#documentation').show();
+        $('#basic-usage').hide();
+        $('ul.nav li').removeClass('active').parent().find('li[data-active="documentation"]').addClass('active');
+    });
+
+    router.get('/', function(){
+        $('#basic-usage').show();
+        $('#documentation').hide();
+        $('ul.nav li').removeClass('active').parent().find('li[data-active="usage"]').addClass('active');
+    });
+
+    $('a[rel]').click(function(e){
+        e.preventDefault();
+        router.navigate($(this).attr('rel'));
+    });
+
+}); 
+},{}]},{},[1]);
