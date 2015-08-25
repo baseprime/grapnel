c5d9dccc40da22823b270d6d4c71622041c30b18
Greg Sabia Tucker <greg@narrowlabs.com>
2015-08-25 13:15:39 -0700

    Changes to url; syntax

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 /****
  * Grapnel Server
- * https://github.com/EngineeringMode/Grapnel.js/tree/server-router
+ * https://github.com/bytecipher/grapnel/tree/server-router
  *
- * @author Greg Sabia Tucker <greg@artificer.io>
- * @link http://artificer.io
- * @version 0.2.1
+ * @author Greg Sabia Tucker <greg@bytecipher.io>
+ * @link http://bytecipher.io
+ * @version 0.2.2
  *
  * Released under MIT License. See LICENSE.txt or http://opensource.org/licenses/MIT
 */
 
 var Grapnel = require('grapnel');
 
-function Server(){
+function Server() {
     Grapnel.apply(this, arguments);
 
     var self = this;
 
-    this.version = '0.2.1';
+    this.version = '0.2.2';
     this.running = false;
     this.verbs = ['GET', 'POST', 'PUT', 'DELETE', 'ALL'];
     // HTTP Verbs
-    this.verbs.forEach(function(verb){
-        self[verb.toLowerCase()] = function(){
+    this.verbs.forEach(function(verb) {
+        self[verb.toLowerCase()] = function() {
             var args = [].slice.call(arguments);
             // Add extra middleware to check if this method matches the requested HTTP verb
             args.splice(1, 0, shouldRun(verb));
@@ -31,7 +31,7 @@ function Server(){
     });
 
     return this;
-}
+};
 
 Server.prototype = Object.create(Grapnel.prototype);
 
@@ -43,12 +43,12 @@ Server.prototype.constructor = Server;
  * @param {[Function]} Middleware (optional)
  * @return {Function} Adds route to context
 */
-Server.prototype.context = function(){
+Server.prototype.context = function() {
     var fn = Grapnel.prototype.context.apply(this, arguments),
         self = this;
 
-    this.verbs.forEach(function(verb){
-        fn[verb.toLowerCase()] = function(){
+    this.verbs.forEach(function(verb) {
+        fn[verb.toLowerCase()] = function() {
             var args = [].slice.call(arguments);
 
             args.splice(1, 0, shouldRun(verb));
@@ -58,72 +58,73 @@ Server.prototype.context = function(){
     });
 
     return fn;
-}
+};
 /**
-* Start listening
-*
-* @return {Function} Middleware
+ * Start listening
+ *
+ * @return {Function} Middleware
 */
-Server.prototype.start = function(){
+Server.prototype.start = function() {
     var router = this;
     // Server should now allow requests
     this.running = true;
     // Return server middleware to HTTP createServer()
-    return function(req, res){
+    return function(req, res) {
         // For every inbound request, we want to map node's req and res to middleware
         Grapnel.CallStack.constructor.globalStack = [serverMiddleware(req, res, router)];
         // Finally, navigate router
         router.navigate(req.url);
     }
-}
+};
 /**
-* Map HTTP createServer request and response to middleware
-*
-* @param {Stream} HTTP Request
-* @param {Stream} HTTP Response
-* @param {Object} Router
-* @return {Function} Middleware
+ * Map HTTP createServer request and response to middleware
+ *
+ * @param {Stream} HTTP Request
+ * @param {Stream} HTTP Response
+ * @param {Object} Router
+ * @return {Function} Middleware
 */
-function serverMiddleware(req, res, router){
+function serverMiddleware(req, res, router) {
 
-    return function wareReqRes(_req, event, next){
+    return function wareReqRes(_req, event, next) {
         // Misc. request parameters should be congruent with Grapnel's `req` parameter conventions
-        for(var prop in _req){
-            if(!req.hasOwnProperty(prop)){
+        for (var prop in _req) {
+            if (!req.hasOwnProperty(prop)) {
                 req[prop] = _req[prop];
             }
         }
         // Create a global request id
         req._id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
         // Event property should now be accessible through the `req` property
         req.event = event;
         // Override next -- this is the same as default event.next() functionality except the arguments are now `req`, `res`, and `next()`
-        req.event.next = function(){
-            return event.stack.shift().call(router, req, res, function(){
+        req.event.next = function() {
+            return event.stack.shift().call(router, req, res, function() {
                 event.next.call(event);
             });
         }
-        
+
         next();
     }
-}
+};
 /**
-* Middleware to check whether or not handler should continue running
-*
-* @param {String} HTTP Method
-* @return {Function} Middleware
+ * Middleware to check whether or not handler should continue running
+ *
+ * @param {String} HTTP Method
+ * @return {Function} Middleware
 */
-function shouldRun(verb){
+function shouldRun(verb) {
     // Add extra middleware to check if this method matches the requested HTTP verb
-    return function wareShouldRun(req, res, next){
+    return function wareShouldRun(req, res, next) {
         var shouldRun = (this.running && (req.method === verb || verb.toLowerCase() === 'all'));
         // Call next in stack if it matches
-        if(shouldRun) next();
+        if (shouldRun) next();
     }
-}
+};
 
 exports = module.exports = new Server();
 exports.Server = Server;
